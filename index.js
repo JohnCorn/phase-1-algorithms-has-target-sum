53ce2f777c8afdc92c3e69fa8e673e1cf1317af0
John Corn <jcornacchioli@gmail.com>
2023-06-20 14:09:00 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for(let i = 0; i < array.length; i++)
+  {
+    for(let a = 0; a < array.length; a++)
+    {
+      if (i == a)
+        continue;
 
+        if ((array[i] + array[a]) == target)
+          return true; 
+    }
+
+  }
+
+  return false;
+}
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
   Add your pseudocode here
+  make a var that holds sum of array
+  loop through array and add each value to var
+  compare var value to target
 */
 
 /*
   Add written explanation of your solution here
+  just comparing the sum to the target, not much more then that
 */
 
 // You can run `node index.js` to view these console logs
